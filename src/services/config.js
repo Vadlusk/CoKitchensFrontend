a90aebcde6bfbae6222b1cfed3df963ba1d06aaa
ben ross <benjamin.nexia@gmail.com>
2021-05-07 08:10:57 -0700

    Determine url based on env

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,8 @@
+const environment = 'development'
+
+const url = () => environment === 'development' ? 'http://localhost:3001' : null
+
 export default {
   apiKey: 'secret_id_you_dont_know',
-  url: 'https://cokitchens.herokuapp.com/api/v1'
+  url
 }
