2da7bba6cb4948cc3d859c792c17392159226a8c
ben ross <benjamin.nexia@gmail.com>
2021-05-08 12:26:18 -0700

    Add responsive menu and drawer functionality

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,25 +1,91 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+
+import { AppBar, Toolbar, Typography, Button, IconButton, Drawer } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 
 import styled from 'styled-components'
 
 const Navbar = () => {
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false
+  })
+  const { mobileView, drawerOpen } = state
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }))
+    }
+    setResponsiveness()
+    window.addEventListener('resize', () => setResponsiveness())
+  }, [])
+
+  const displayDesktop = () => {
+    return (
+      <Toolbar>
+        <Typography color="inherit">
+          <S.Link href="/" color="inherit">CoKitchens</S.Link>
+        </Typography>
+        <S.Menu>
+          <Button href="/calendar" color="inherit">Calendar</Button>
+          <Button href="/toolkit" color="inherit"> Toolkit </Button>
+          <Button href="/kitchens" color="inherit">Kitchens</Button>
+          <Button href="/stories" color="inherit"> Stories </Button>
+          <Button href="/team" color="inherit">    Team    </Button>
+          <Button href="/contact" color="inherit"> Contact </Button>
+        </S.Menu>
+      </Toolbar>
+    )
+  }
+
+  const displayMobile = () => {
+    const handleDrawerOpen = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: true }))
+    const handleDrawerClose = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: false }))
+
+    return (
+      <>
+      <IconButton
+        {...{
+          edge: 'end',
+          color: 'inherit',
+          'aria-label': 'menu',
+          'aria-haspopup': 'true',
+          onClick: handleDrawerOpen
+        }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+         {...{
+           anchor: 'left',
+           open: drawerOpen,
+           onClose: handleDrawerClose
+         }}
+       >
+         <div>
+           <S.Menu>
+             <Button href="/calendar" color="inherit">Calendar</Button>
+             <Button href="/toolkit" color="inherit"> Toolkit </Button>
+             <Button href="/kitchens" color="inherit">Kitchens</Button>
+             <Button href="/stories" color="inherit"> Stories </Button>
+             <Button href="/team" color="inherit">    Team    </Button>
+             <Button href="/contact" color="inherit"> Contact </Button>
+           </S.Menu>
+         </div>
+       </Drawer>
+      <S.Link href="/" color="inherit">CoKitchens</S.Link>
+      </>
+    )
+  }
+
   return (
     <div>
       <AppBar position="static">
-          <Toolbar>
-            <Typography color="inherit">
-              <S.Link>CoKitchens</S.Link>
-            </Typography>
-            <div>
-              <Button href="/calendar" color="inherit">Calendar</Button>
-              <Button href="/toolkit" color="inherit"> Toolkit </Button>
-              <Button href="/kitchens" color="inherit">Kitchens</Button>
-              <Button href="/stories" color="inherit"> Stories </Button>
-              <Button href="/team" color="inherit">    Team    </Button>
-              <Button href="/contact" color="inherit"> Contact </Button>
-            </div>
-          </Toolbar>
+        {mobileView ? displayMobile() : displayDesktop()}
       </AppBar>
     </div>
   )
@@ -29,5 +95,10 @@ export default Navbar
 
 const S = {
   Link: styled.a`
+  color: white;
+  text-decoration: none;
+  `,
+  Menu: styled.div`
+  margin: auto;
   `
 }
