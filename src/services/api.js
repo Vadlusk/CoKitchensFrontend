697ba516bd243be4026a7353abc77377357183af
ben ross <benjamin.nexia@gmail.com>
2021-05-15 14:39:02 -0700

    Remove config file and use .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
-import config from './config'
-
 const fetchAPI = (path, method = 'GET', payload = null) => {
   const body = payload ? JSON.stringify(payload) : null
 
-  return fetch(`${config.url()}${'/api/v1' + path}`, {
+  return fetch(`${process.env.REACT_APP_BASE_BACKEND_URL}${'/api/v1' + path}`, {
     method,
     headers: {
       'Content-Type': 'application/json',
--- src/services/config.js
+++
@@ -1,8 +0,0 @@
-const environment = 'development'
-
-const url = () => environment === 'development' ? 'http://localhost:3001' : null
-
-export default {
-  apiKey: 'secret_id_you_dont_know',
-  url
-}
